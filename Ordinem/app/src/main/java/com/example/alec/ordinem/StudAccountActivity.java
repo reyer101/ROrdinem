a35d5e0eaea5f737a6dac7725fa88ae58ffcd189
Alec <reyer101@mail.chapman.edu>
2017-01-25 15:47:53 -0800

    Initial Commit

---
+++ Ordinem/app/src/main/java/com/example/alec/ordinem/StudAccountActivity.java
@@ -0,0 +1,13 @@
+package com.example.alec.ordinem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stud_account);
+    }
+}
